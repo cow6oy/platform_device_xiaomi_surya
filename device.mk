a7583f76b849d6ec3986fbbc2fd36e7d75bef7fc
Cow6oy <mezackisilva@gmail.com>
2022-07-20 09:30:08 -0300

    surya: overlay: Move FP sensor location overlays
    Change-Id: Ib21d79b955d7dffaa4afd66c83cc111a47ede14f

--- device.mk
+++ device.mk
@@ -326,7 +326,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
